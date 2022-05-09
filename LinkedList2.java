14f2b2cd3fb6fc485cc1928291a63bd69ea96176
Polumatisrilakshmi <polumatisrilakshmi@gmail.com>
2022-05-09 11:36:06 +0530

    Added:ChangedSeqenceAgain

---
+++ LinkedList2.java
@@ -0,0 +1,37 @@
+package COM.BRIDGELABZ;
+import java.util.LinkedList;
+public class LinkedList2 {
+    public static class Node{
+        Node next;
+        int data;
+
+        public void data(int data) {
+            this.data = data;
+        }
+
+    }
+    private void show() {
+        Node newNode1 = new Node();
+        newNode1.data(56);
+
+        Node newNode2 = new Node();
+        newNode2.data(30);
+        newNode1.next = newNode2;
+
+        Node newNode3 = new Node();
+        newNode3.data(70);
+        newNode2.next = newNode3;
+
+        System.out.println(newNode1.data + "-->" + newNode2.data + "-->" + newNode3.data);
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("my linklist is : ");
+        LinkedList2 list = new LinkedList2();
+        list.show();
+
+    }
+
+}
+
